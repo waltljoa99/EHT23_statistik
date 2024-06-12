090e221001c447b69c0130fba7f4c9cd5c6cb58b
KaleidoScoper <85396661+Stefan-AnShin@users.noreply.github.com>
2024-06-12 21:37:41 +0200

    Add function to calculate accuracy of clutering

--- ue5/helper.R
+++ ue5/helper.R
@@ -41,3 +41,79 @@ create_histograms <- function(data) {
 ########################################################################################
 # Helper functions for the exercises
 ########################################################################################
+
+#' Calculate the accuracy of clustering
+#'
+#' @param group_data data.frame: data frame containing the original group labels
+#' @param result_data list: list containing the clustering results
+#' @param groups character: vector containing the names of the groups
+#'
+#' @return accuracy of the clustering results compared to the original group labels
+calculate_accuracy <- function(group_data, result_data, groups) {
+  # Convert the group names to factors
+  group_data$group <- factor(group_data$group, levels = groups)
+
+  # Calculate the means of the groups
+  group_means <- aggregate(. ~ group, group_data, mean)
+
+  # Create a data frame containing the cluster means
+  cluster_means <- get_cluster_means(group_data, result_data)
+
+  # Initialize an empty vector to store the groups
+  clusters <- c()
+
+  # Drop group column
+  group_means <- dplyr::select(group_means, -group)
+  cluster_means <- dplyr::select(cluster_means, -cluster)
+
+  # Loop over all rows of group_means
+  for (i in 1:nrow(group_means)) {
+    # Get the i-th row of the dataframe
+    group_means_row <- group_means[i, ]
+
+    # Overwrite all rows with the i-th row
+    group_means_rows <- do.call("rbind", replicate(nrow(group_means), group_means_row, simplify = FALSE))
+
+    # Calculate the squared differences
+    differences <- (group_means_rows - cluster_means)^2
+
+    # Calculate the Euclidean distances
+    euclidian_distances <- sqrt(rowSums(differences))
+
+    # Find the cluster mean with the smallest distance to the given group mean
+    clusters[i] <- which.min(euclidian_distances)
+  }
+
+  # Create a named vector for recoding
+  recode_vector <- setNames(clusters, groups)
+
+  # Recode the 'group' variable to the matching cluster
+  group <- recode(group_data$group, !!!recode_vector)
+
+  # Compare the accuracy of the clustering results with the ground truth
+  accuracy <- sum(result_data$cluster == group) / nrow(group_data)
+
+  return(accuracy)
+}
+
+#' Calculate the means of the clusters
+#'
+#' @param group_data data.frame: data frame containing the original group labels
+#' @param result_data list: list containing the clustering results.
+#'
+#' @return data.frame: data frame containing the means of each cluster.
+get_cluster_means <- function(group_data, result_data) {
+  # Drop the 'group' column from the dataframe
+  # This is done because we want to calculate the means of the clusters, not the original groups
+  cluster_data <- dplyr::select(group_data, -group)
+
+  # Add the cluster assignments to the dataframe
+  cluster_data$cluster <- result_data$cluster
+
+  # Calculate the means of the clusters
+  # The aggregate function is used to calculate the mean of each variable for each cluster
+  cluster_means <- aggregate(. ~ cluster, cluster_data, mean)
+
+  # Return the means of the clusters
+  return(cluster_means)
+}
